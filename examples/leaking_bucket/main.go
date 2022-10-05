f903d82bf98c2919957e2c8dca9a3322dd8ce708
Federico Ragona <federico.ragona@gmail.com>
2022-10-05 19:23:16 +0200

    Add usage examples

---
+++ examples/leaking_bucket/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/fedragon/rate-limiter/common"
+	"github.com/fedragon/rate-limiter/leaking_bucket"
+)
+
+func main() {
+	rate := &common.Rate{
+		Value:    2,
+		Interval: time.Minute,
+	}
+	rateLimiter := leaking_bucket.NewRateLimiter(rate)
+	defer rateLimiter.Stop()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt)
+
+	handler := rateLimiter.Handle(
+		http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			return
+		}),
+	)
+
+	go func() {
+		if err := http.ListenAndServe("0.0.0.0:3000", handler); err != nil {
+			log.Fatal(err)
+			return
+		}
+	}()
+
+	<-shutdown
+}
